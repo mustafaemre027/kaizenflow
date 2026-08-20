9893e4b56bf6c0bf3fefd113e43aa72726eed077
Mustafa emre kaplan <mustafaemre027@gmail.com>
2026-08-20 17:13:24 +0300

    chore: remove scratch files

--- scratch_sqlite_rollback.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Artisan::call('migrate:fresh');
-Artisan::call('migrate:rollback', ['--step' => 2]);
-echo json_encode(DB::select("SELECT name, sql FROM sqlite_master WHERE type = 'table' AND name IN ('user_capability_grants', 'user_system_capability_grants')"))."\n";
