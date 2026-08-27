b5fffa60b1a7b4fec798c3abfd6afd2c992c3884
Mustafa emre kaplan <mustafaemre027@gmail.com>
2026-08-27 13:50:34 +0300

    feat(auth): add explicit translations for password reset form validations

--- lang/tr/validation.php
+++ lang/tr/validation.php
@@ -2,6 +2,8 @@
 
 return [
     'email' => 'Lütfen geçerli bir e-posta adresi girin.',
+    'required' => 'Bu alanın doldurulması zorunludur.',
+    'confirmed' => 'Yeni parola ile parola tekrarı eşleşmiyor.',
     'min' => [
         'string' => 'Yeni parola en az :min karakter olmalıdır.',
     ],
